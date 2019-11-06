fdaf0690db31e47288f805c1d336edc3f376bce2
killeraliens <killeraliens@outlook.com>
2019-11-05 20:42:01 -0700

    express playground

---
+++ app.js
@@ -0,0 +1,74 @@
+// const fs = require('fs');
+// console.log("directory name:", __dirname);
+// console.log("process env", process.env);
+// const myReadMe = fs.readFileSync('../alexandra-brinn/README.md', 'utf-8')
+// console.log(myReadMe);
+
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+app.use(morgan('dev'))
+app.use(express.json())
+
+app.listen(8000, () => {
+  console.log('express arena listening on port 8000');
+})
+
+app.get('/', (req, res) => {
+  res.send('public messages')
+})
+
+app.get('/convos', (req, res) => {
+  res.send('your conversations')
+})
+
+app.get('/echo', (req, res) => {
+  const reqText = `
+    Request object details...
+    baseUrl: ${req.baseUrl},
+    hostname: ${req.hostname},
+    path: ${req.path},
+    ip: ${req.ip},
+    fresh?: ${req.fresh},
+    method: ${req.method} ,
+    params: ${Object.keys(req.params).join(' ')},
+    body: ${req.body}
+  `;
+
+  res.send(reqText);
+
+})
+
+app.get('/queryViewer', (req, res) => {
+  // console.log(req.query)
+
+  const name = req.query.name;
+  const race = req.query.race;
+
+  if (!name) {
+    return res.status(400).send('Please provide name')
+  }
+
+  if (!race) {
+    return res.status(400).send('Please provide race')
+  }
+
+  const greeting = `Greetings ${name} of the ${race} people`;
+  console.log(req.cookies);
+  res.send(greeting)
+})
+
+app.get('/sum', (req, res) => {
+  const a = req.query.a;
+  const b = req.query.b;
+
+  if (a && b ) {
+    const sum = parseInt(a, 10) + parseInt(b, 10);
+
+    //res.send(`the sum of ${a} and ${b} = ${sum}`)
+    isNaN(sum)
+     ? res.status(400).send('Must be two numbers please')
+     : res.send(`the sum of ${a} and ${b} = ${sum}`)
+  }
+  res.status(400).send('Set a and b values with numbers')
+})
